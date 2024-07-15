63086804049fe7211bb0ed9919235406ae6dd16d
Crowdbotics <team@crowdbotics.com>
2024-07-15 00:49:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crochet-genius-48746",
+  url: "https://crochet-genius-48746.botics.co",
+  api: "https://crochet-genius-48746.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
